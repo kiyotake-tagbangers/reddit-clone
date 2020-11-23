b625da66894f528350baee958857c253e7b2c281
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2020-11-24 01:03:44 +0900

    user login activation

--- src/main/java/com/kiyotagbangers/redditclone/exceptions/SpringRedditException.java
+++ src/main/java/com/kiyotagbangers/redditclone/exceptions/SpringRedditException.java
@@ -6,5 +6,9 @@ public class SpringRedditException extends RuntimeException {
     public SpringRedditException(String exMessage, Exception exception) {
         super(exMessage, exception);
     }
+
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
 }
 
--- src/main/java/com/kiyotagbangers/redditclone/service/AuthService.java
+++ src/main/java/com/kiyotagbangers/redditclone/service/AuthService.java
@@ -1,28 +1,35 @@
 package com.kiyotagbangers.redditclone.service;
 
 import com.kiyotagbangers.redditclone.dto.RegisterRequest;
+import com.kiyotagbangers.redditclone.exceptions.SpringRedditException;
 import com.kiyotagbangers.redditclone.model.NotificationEmail;
 import com.kiyotagbangers.redditclone.model.User;
 import com.kiyotagbangers.redditclone.model.VerificationToken;
 import com.kiyotagbangers.redditclone.repository.UserRepository;
 import com.kiyotagbangers.redditclone.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final VerificationTokenRepository verificationTokenRepository;
     private final MailService mailService;
+
+    @Value("${server.port}")
+    private String serverPort;
 
     @Transactional
     public void signup(RegisterRequest registerRequest){
@@ -36,10 +43,11 @@ public class AuthService {
         userRepository.save(user);
 
         String token = generateVerificationToken(user);
+
         mailService.sendMail(new NotificationEmail("Please Activate your Account",
                 user.getEmail(), "Thank you for signing up to Spring Reddit, " +
                 "please click on the below url to activate your account : " +
-                "http://localhost:8080/api/auth/accountVerification/" + token));
+                "http://localhost:" + serverPort + "/api/auth/accountVerification/" + token));
     }
 
     private String generateVerificationToken(User user) {
@@ -50,5 +58,21 @@ public class AuthService {
 
         verificationTokenRepository.save(verificationToken);;
         return token;
+    }
+
+    public void verifyAccount(String token) {
+        Optional<VerificationToken> verificationToken = verificationTokenRepository.findByToken(token);
+        verificationToken.orElseThrow(
+                () -> new SpringRedditException("Invalid Token"));
+        fetchUserAndEnable(verificationToken.get());
+    }
+
+    @Transactional
+    private void fetchUserAndEnable(VerificationToken verificationToken) {
+        String username = verificationToken.getUser().getUsername();
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new SpringRedditException("User not found with name - " + username));
+        user.setEnabled(true);
+        userRepository.save(user);
     }
 }
